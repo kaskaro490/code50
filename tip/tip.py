1304a18d096044334377f3f97413638a66384f4b
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-06 18:32:31 +0000

    Sat, Jul 6, 2024, 6:32 PM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -14,3 +14,10 @@ def percent_to_float(p):
 
 
 main()
+
+
+
+dollars_to_float, which should accept a str as input (formatted as $##.##, wherein each # is a decimal digit), remove the leading $,
+and return the amount as a float. For instance, given $50.00 as input, it should return 50.0.
+percent_to_float, which should accept a str as input (formatted as ##%,
+wherein each # is a decimal digit), remove the trailing %, and return the percentage as a float. For instance, given 15% as input, it should return 0.15.
