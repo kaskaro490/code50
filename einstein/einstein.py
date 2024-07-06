c2907a7dda36dd84682e150491e62d9b4c15c0a9
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-06 17:20:18 +0000

    Sat, Jul 6, 2024, 5:20 PM +00:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -2,7 +2,7 @@ def main():
     m = input("Give the mass of an object (in kilograms):")
     c2 = 300000000*300000000
     E = m*c2
-    print("The number of joules is: " + E)
+    print(f"The number of joules is: " + E)
 
 
 main()
