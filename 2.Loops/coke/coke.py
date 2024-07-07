f3d3a34c038c5b660806179198e577a5b95d35a1
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 21:18:07 +0000

    Sun, Jul 7, 2024, 11:18 PM +02:00

--- 2.Loops/coke/coke.py
+++ 2.Loops/coke/coke.py
@@ -2,7 +2,7 @@ def main():
   okcoins = ["5","10","25"]
   due = 50
   while due > 0:
-    insert_coin = int(input("Insert a coin"))
+    insert_coin = int(input("Insert a coin: "))
     if insert_coin in okcoins:
       due -= insert_coin
       print("Amount due: " + due)
