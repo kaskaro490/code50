730a6c1c2f4a2372fcc5dc67fea08037eba039c4
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 12:15:30 +0000

    Sun, Jul 7, 2024, 12:15 PM +00:00

--- 1.Conditions/extensions/extensions.py
+++ 1.Conditions/extensions/extensions.py
@@ -1,13 +1,27 @@
 filename = input("File name: ").lower()
 
+if filename.endswith(.gif):
+    print("image/gif")
+elif filename.endswith(.jpg):
+    print("image/jpg")
+elif filename.endswith(.jpeg):
+    print("image/jpg")
+elif filename.endswith(.png):
+    print("image/png")
+elif filename.endswith(.pdf):
+    print("application/pdf")
+elif filename.endswith(.zip):
+    print("application/zip")
+elif filename.endswith(.txt):
+    print("text/plain")
+
+else:
+    print("application/octet-stream")
 
 
 
-image/gif
-image/jpeg
-image/png
-image/png
-application/pdf
-application/zip
-application/octet-stream
-text/plain
+
+
+
+
+
