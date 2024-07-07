cb73eb69e98bfcba7e3bf24f1a9eb37b6715f12f
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 13:05:27 +0000

    Sun, Jul 7, 2024, 1:05 PM +00:00

--- 1.Conditions/meal/meal.py
+++ 1.Conditions/meal/meal.py
@@ -5,8 +5,11 @@ breakfast time, lunch time, or dinner time
 
 def main():
     usertime = input("What time it is? ")
-    
+    x,y = usertime.split(":")
+    x = int(x)
+    y = int(y)
 
+    result = 
 
 def convert(time):
     ...
