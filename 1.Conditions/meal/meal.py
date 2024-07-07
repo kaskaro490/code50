78e8c4eee6d4672d480718c2cc6e2b1444180c70
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 13:27:08 +0000

    Sun, Jul 7, 2024, 1:27 PM +00:00

--- 1.Conditions/meal/meal.py
+++ 1.Conditions/meal/meal.py
@@ -5,14 +5,14 @@ def main():
     x = int(x)
     y = int(y)
 
-    answer = time.wichmeal()
+    answer = convert().wichmeal()
     print(answer)
 
 def convert(time):
     yfloat = float(((y*100)/60)/100)
     time = float(x + yfloat)
 
-def wichmeal(time):
+def wichmeal():
     if 7 <= time <= 8:
         print("breakfast time")
     elif 12 <= time <= 13:
