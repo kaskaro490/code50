f1e5a852aa45c13fa7b31b951bae7936fcb3979f
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 12:41:16 +0000

    Sun, Jul 7, 2024, 12:41 PM +00:00

--- 1.Conditions/interpreter/interpreter.py
+++ 1.Conditions/interpreter/interpreter.py
@@ -3,11 +3,13 @@ def main():
     expression = input("Expression: ")
 
     x, y, z = expression.split(" ")
+    x = float(x)
+    z = float(z)
 
     print(expression)
 
 
-def interpreter():
+def interpreter(x, y, z):
     if y == "+":
         return x + z
 
