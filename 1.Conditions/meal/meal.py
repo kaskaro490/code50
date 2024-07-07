7aef1511c2d6087195c392acca3fa3f2c05f2fbc
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 13:03:01 +0000

    Sun, Jul 7, 2024, 1:03 PM +00:00

--- 1.Conditions/meal/meal.py
+++ 1.Conditions/meal/meal.py
@@ -1,3 +1,16 @@
 breakfast time, lunch time, or dinner time
 
 24-hour time
+
+
+def main():
+    usertime = input("What time it is? ")
+    
+
+
+def convert(time):
+    ...
+
+
+if __name__ == "__main__":
+    main()
