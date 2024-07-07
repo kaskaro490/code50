e5abed89a81d4abd7d113e71bd41c03dc348967b
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 13:37:56 +0000

    Sun, Jul 7, 2024, 1:37 PM +00:00

--- 1.Conditions/meal/meal.py
+++ 1.Conditions/meal/meal.py
@@ -1,25 +1,21 @@
 
 def main():
-    time = input("What time it is? ")
+    time = convert(input("What time it is? "))
 
-
-    meal = convert(time).wichmeal(time)
-    print(answer)
-
-def convert(time):
-    x,y = time.split(":")
-    x = int(x)
-    y = int(y)
-    yfloat = float(((y*100)/60)/100)
-    time = float(x + yfloat)
-
-def wichmeal(time):
     if 7 <= time <= 8:
         print("breakfast time")
     elif 12 <= time <= 13:
         print("lunch time")
     elif 18 <= time <= 19:
         print("dinner time")
+
+def convert(time):
+    x,y = time.split(":")
+    x = int(x)
+    y = int(y)
+    yfloat = float(((y*100)/60)/100)
+    return float(x + yfloat)
+
 
 
 if __name__ == "__main__":
