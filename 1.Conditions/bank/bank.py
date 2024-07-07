507ba8e0b72e6fddfd031a4912da240c61996efa
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 11:33:36 +0000

    Sun, Jul 7, 2024, 11:33 AM +00:00

--- 1.Conditions/bank/bank.py
+++ 1.Conditions/bank/bank.py
@@ -1,3 +1,8 @@
 answer = input("Greeting: ").lower()
+firstletter = answer[0]
 
-if answer 
+if answer[0] == "h":
+    if answer == "hello":
+        print("$100")
+    else: print("$20")
+else: print("$0")
