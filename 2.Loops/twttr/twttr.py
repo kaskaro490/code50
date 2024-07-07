2c365959b496bf479f942ba295fa6e60159da49e
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-07 22:29:18 +0000

    Mon, Jul 8, 2024, 12:29 AM +02:00

---
+++ 2.Loops/twttr/twttr.py
@@ -0,0 +1,13 @@
+def main():
+   vowels = [A,E,I,O,U,a,e,i,o,u]
+   input = input("Input: ")
+   print(outputlist)
+def twttr(input):
+   outputlist = []
+   for letter in input:
+      if not letter in vowels:
+         letter.append(outputlist)
+
+   return ''. join(outputlist)
+
+main()
