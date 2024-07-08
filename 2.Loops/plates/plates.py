13315c7bcfa1ff3b293dec6f0694023cb4cf433e
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-08 09:17:46 +0000

    Mon, Jul 8, 2024, 11:17 AM +02:00

--- 2.Loops/plates/plates.py
+++ 2.Loops/plates/plates.py
@@ -1,27 +1,29 @@
 # Demander le nom de plaque de l'utilisateur et lui donne rle résultat
 def main():
     plate = input("Plate: ").upper()
-    letters:
-
     charnumber = len(plate)
+
     if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
 #Vérifier qu'un nom de plaque est valide sur tous les paramètres
-def is_valid(s):
+def is_valid(s, plate):
     #Vérifier que le nom contient bien entre 2 et 6 caractères
     if 2<= charnumber <=6:
-        for s in plate:
-            if plate[:2].isalpha():
+        #Vérifier que le début du nom contient bien 2 lettres
+        if plate[:2].isalpha():
+            #Vérifier que le nom n'a pas de chiffre entre les lettres
+            index = 0
+            for s in plate:
+                if s
 
-            else: 
 
 
-#Vérifier que le nom commence bien par deux lettres
 
-#Vérifier que le nom n'a pas de chiffre entre les lettres
+
+
 
 main()
 
