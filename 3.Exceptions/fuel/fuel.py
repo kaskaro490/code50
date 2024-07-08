6eae16067537548dfba2614329f3ba013084660b
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-08 19:19:01 +0000

    Mon, Jul 8, 2024, 9:19 PM +02:00

--- 3.Exceptions/fuel/fuel.py
+++ 3.Exceptions/fuel/fuel.py
@@ -5,12 +5,11 @@ def main():
             fraction = input("Fraction: ")
             # Split l'input en X/Y
             x, y = fraction.split("/")
-            x = int(round(float(x)))
-            y = int(round(float(y)))
+
             print(f"{x} and {y}")
 
             # Convertir la fraction en pourcentage arrondi à l'intégral le plus proche
-            percentage = (x*100)/y
+            percentage = int(round(x*100)/y))
 
 
             # Boucler et redemander un input si l'input est supérieur à 100% ou en cas de  ZeroDivisionError et ValueError
