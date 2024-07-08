5552ae3b8e17a69bdf49faef58b85ae862c558ba
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-08 16:00:50 +0000

    Mon, Jul 8, 2024, 6:00 PM +02:00

--- 2.Loops/nutrition/nutrition.py
+++ 2.Loops/nutrition/nutrition.py
@@ -1,30 +1,31 @@
 def main():
 
-    FruitCalories = {
-"Apple": "130",
-"Avocado": "50",
-"Banana": "110",
-"Cantaloupe": "50",
-"Grapefruit": "60",
-"Grapes": "90",
-"Honeydew melon": "50",
-"Kiwifruit": "90",
-"Lemon": "15",
-"Lime": "20",
-"Nectarine": "60",
-"Orange": "80",
-"Peach": "60",
-"Pear": "100",
-"Pineapple": "50",
-"Plums": "70",
-"Strawberries": "50",
-"Sweet cherries": "100",
-"Tangerine": "50",
-"Watermelon": "80"
-}
+    fruitcalories = {
+    "Apple": "130",
+    "Avocado": "50",
+    "Banana": "110",
+    "Cantaloupe": "50",
+    "Grapefruit": "60",
+    "Grapes": "90",
+    "Honeydew melon": "50",
+    "Kiwifruit": "90",
+    "Lemon": "15",
+    "Lime": "20",
+    "Nectarine": "60",
+    "Orange": "80",
+    "Peach": "60",
+    "Pear": "100",
+    "Pineapple": "50",
+    "Plums": "70",
+    "Strawberries": "50",
+    "Sweet cherries": "100",
+    "Tangerine": "50",
+    "Watermelon": "80"
+    }
 
     Fruit = input("Fruit: ").lower()
-    print(FruitCalories[{Fruit}])
+    for fruits in fruitcalories:
+        print(fruits["{Fruit}"])
 
 
 main()
