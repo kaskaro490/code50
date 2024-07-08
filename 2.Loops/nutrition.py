0a5729e61a3ae2ecf44728baedf80a654752b770
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-08 15:46:35 +0000

    Mon, Jul 8, 2024, 5:46 PM +02:00

--- 2.Loops/nutrition.py
+++ 2.Loops/nutrition.py
@@ -1,6 +1,26 @@
 def main():
-    
 
-
+FruitCalories = [
+Apple 130
+Avocado 50
+Banana 110
+Cantaloupe 50
+Grapefruit 60
+Grapes 90
+Honeydew melon 50
+Kiwifruit 90
+Lemon 15
+Lime 20
+Nectarine 60
+Orange 80
+Peach 60
+Pear 100
+Pineapple 50
+Plums 70
+Strawberries 50
+Sweet cherries 100
+Tangerine 50
+Watermelon 80
+]
 
 main()
