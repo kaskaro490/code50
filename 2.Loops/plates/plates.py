22b08ae4d5cd3b41ed3ed3dc0abc4700d8475610
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-08 11:39:46 +0000

    Mon, Jul 8, 2024, 1:39 PM +02:00

--- 2.Loops/plates/plates.py
+++ 2.Loops/plates/plates.py
@@ -13,30 +13,30 @@ def is_valid(s):
     # Vérifier si toutes les conditions sont remplies
     return len_plate_minmax(s) and first_2_letters(s) and no_middle_numbers(s)
 
-def len_plate_minmax(x):
+def len_plate_minmax(s):
     #Vérifier que le nom contienne bien entre 2 et 6 caractères
-    if 2<= len(x) <=6:
+    if 2<= len(s) <=6:
         return True
     else:
         return False
 
-def first_2_letters(y):
+def first_2_letters(s):
     #Vérifier que le début du nom contient bien 2 lettres
-    if y[:2].isalpha():
+    if s[:2].isalpha():
         return True
     else:
         return False
 
-def no_middle_numbers(z):
-    for i in range(2, len(z) - 1):
-        if z[i].isdigit():
+def no_middle_numbers(s):
+    for i in range(2, len(s) - 1):
+        if s[i].isdigit():
             start = i
-            while i < len(z) and z[i].isdigit():
+            while i < len(s) and s[i].isdigit():
                i += 1
             end = i
-            if z[start -1].isalpha() and z[end].isalpha():
+            if s[start -1].isalpha() and s[end].isalpha():
                 return False
-            elif z[start == 0]:
+            elif s[start == 0]:
                 return False
 
 
