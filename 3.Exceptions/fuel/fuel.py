c3d9750f0e3a28fc09a851e3df5913b717a377fc
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-09 16:44:59 +0000

    Tue, Jul 9, 2024, 6:44 PM +02:00

--- 3.Exceptions/fuel/fuel.py
+++ 3.Exceptions/fuel/fuel.py
@@ -15,22 +15,21 @@ def userfraction():
             fraction = input("Fraction: ") # Demander d'entrer une fraction
             x, y = fraction.split("/") # Split l'input X/Y
 
+            x = int(x)
+            y = int(y)
             if x <= y:
-                x = int(x)
-                y = int(y)
+                return int(round((x*100)/y)) # Convertir et retourner la fraction en pourcentage arrondi à l'intégral le plus proche
 
 
 
-
-            # Boucler et redemander un input si l'input est supérieur à 100% ou en cas de  ZeroDivisionError et ValueError
 
         except (ValueError, ZeroDivisionError) as e:
             print(f"{e}")
 
         except x > y:
-            break
+            pass
 
-        else:
-            return int(round((x*100)/y)) # Convertir et retourner la fraction en pourcentage arrondi à l'intégral le plus proche
+
+
 main()
 
