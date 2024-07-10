1092c38b9fa411f5f479307fe4d65668b815eb09
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-10 11:40:52 +0000

    Wed, Jul 10, 2024, 1:40 PM +02:00

--- 3.Exceptions/taqueria/taqueria.py
+++ 3.Exceptions/taqueria/taqueria.py
@@ -19,11 +19,11 @@ def main():
 
 
 def totalcommand(menulower, total): # Fonction de prise de commande
-    while item = input("Item: ").lower(): # Ajout d'un item:
+    while True:
 
         try:
 
-
+            item = input("Item: ").lower() # Ajout d'un item
             if item in menulower:
                 total += menulower[item] # ajout du prix de l'article au total de la commande
 
