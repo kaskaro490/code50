3486eb9f02a7010ce105a79984dd030db01d36aa
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-10 21:10:52 +0000

    Wed, Jul 10, 2024, 11:10 PM +02:00

--- 3.Exceptions/outdated/outdated.py
+++ 3.Exceptions/outdated/outdated.py
@@ -16,6 +16,24 @@ def main():
 ]
 
 
+# Demander à l'utilisateur d'entrer une date au format `MM/DD/YYYY` ou `Month DD, YYYY`.
+
+
+# Vérifier si l'entrée de l'utilisateur correspond à l'un des deux formats de date (numérique ou textuel).
+
+# Si l'entrée n'est pas valide, demander de nouveau à l'utilisateur jusqu'à obtenir une date valide.
+
+# Si la date est au format `MM/DD/YYYY`, la diviser en trois parties : mois, jour et année.
+# Si la date est au format `Month DD, YYYY`, convertir le mois en son équivalent numérique (par exemple, "January" en "01").
+# Réorganiser les parties de la date en format `YYYY-MM-DD`.
+
+# S'assurer que les mois et les jours sont formatés avec deux chiffres, en ajoutant des zéros non significatifs si nécessaire (par exemple, "9" devient "09").
+
+# Afficher la date au format `YYYY-MM-DD`.
+
+# Utiliser des méthodes de chaînes de caractères comme `split`.
+# Utiliser des méthodes de liste comme `index`.
+# Utiliser la syntaxe de formatage des chaînes pour ajouter des zéros non significatifs.
 
 
 
