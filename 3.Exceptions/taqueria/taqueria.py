0cdf0af580312f760ccfeef41abd9cc5cae3c3e0
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-10 11:38:32 +0000

    Wed, Jul 10, 2024, 1:38 PM +02:00

--- 3.Exceptions/taqueria/taqueria.py
+++ 3.Exceptions/taqueria/taqueria.py
@@ -20,10 +20,8 @@ def main():
 
 def totalcommand(menulower, total): # Fonction de prise de commande
     while True:
-
+        
         try:
-
-            item = input("Item: ").lower() # Ajout d'un item
             if item in menulower:
                 total += menulower[item] # ajout du prix de l'article au total de la commande
 
@@ -31,7 +29,7 @@ def totalcommand(menulower, total): # Fonction de prise de commande
 
         except EOFError:
             return print(f"Total: ${total:.2f}")
-
+            break
         except:
             pass
 
