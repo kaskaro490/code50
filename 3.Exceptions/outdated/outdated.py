f76165d52c6dc897fc93472f915a94cbd29a93b7
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-11 11:50:19 +0000

    Thu, Jul 11, 2024, 1:50 PM +02:00

--- 3.Exceptions/outdated/outdated.py
+++ 3.Exceptions/outdated/outdated.py
@@ -30,11 +30,11 @@ def main():
 # Vérifier si l'entrée de l'utilisateur correspond à l'un des deux formats de date (numérique ou textuel).
 def check_date(userdate, months):
     pattern = r'^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/(\d{4})$'
-    pattern2 = rf'({months}),(0[1-9]|[12][0-9]|3[01])/(\d{4})$'
+    pattern2 = rf'({months}),(0?[1-9]|[12][0-9]|3[01])/(\d{4})$'
 
     # Si la date est au format `MM/DD/YYYY`, la diviser en trois parties : mois, jour et année.
     if re.match(pattern, userdate):
-        
+
 
     # Si la date est au format `Month DD, YYYY`, convertir le mois en son équivalent numérique (par exemple, "January" en "01").
     elif re.match(pattern2, userdate, months):
