5d037e49adacf4da286c92759541c680f75ffdcf
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-11 12:27:45 +0000

    Thu, Jul 11, 2024, 2:27 PM +02:00

--- 3.Exceptions/outdated/outdated.py
+++ 3.Exceptions/outdated/outdated.py
@@ -17,16 +17,17 @@ def main():
     "December"
 ]
 
-
-    try:
-# Demander à l'utilisateur d'entrer une date au format `MM/DD/YYYY` ou `Month DD, YYYY`.
-        userdate = input("Date: ")
-        check_date(userdate, months)
-
-
-    except:
+    while True:
+            try:
+        # Demander à l'utilisateur d'entrer une date au format `MM/DD/YYYY` ou `Month DD, YYYY`.
+                userdate = input("Date: ")
+                check_date(userdate, months)
 
 
+            except:
+                pass # Si l'entrée n'est pas valide, demander de nouveau à l'utilisateur jusqu'à obtenir une date valide.
+
+    print() # Réorganiser les parties de la date en format `YYYY-MM-DD`.
 
 # Vérifier si l'entrée de l'utilisateur correspond à l'un des deux formats de date (numérique ou textuel).
 def check_date(userdate, months):
@@ -38,7 +39,9 @@ def check_date(userdate, months):
         month = pattern.group(1)  # Mois
         day = pattern.group(2)  # Jour
         year = pattern.group(3)  # Année
-        return x,y,z
+
+        return month, day, year
+
     # Si la date est au format `Month DD, YYYY`, convertir le mois en son équivalent numérique (par exemple, "January" en "01").
     elif re.match(pattern2, userdate, months, re.IGNORECASE):
         month_word = pattern2.group(1)  # Mois en lettres
@@ -46,14 +49,16 @@ def check_date(userdate, months):
         day = pattern2.group(2)  # Jour
         year = pattern2.group(3)  # Année
 
-
-# Si l'entrée n'est pas valide, demander de nouveau à l'utilisateur jusqu'à obtenir une date valide.
-
+        return month, day, year
 
 
-# Réorganiser les parties de la date en format `YYYY-MM-DD`.
 
-# S'assurer que les mois et les jours sont formatés avec deux chiffres, en ajoutant des zéros non significatifs si nécessaire (par exemple, "9" devient "09").
+
+
+
+
+
+
 
 # Afficher la date au format `YYYY-MM-DD`.
 
