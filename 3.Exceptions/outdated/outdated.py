fdfd33f62a4a33b97d16fd139e7ee01659ab8ad6
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-12 16:12:13 +0000

    Fri, Jul 12, 2024, 6:12 PM +02:00

--- 3.Exceptions/outdated/outdated.py
+++ 3.Exceptions/outdated/outdated.py
@@ -51,7 +51,7 @@ def check_date(userdate, months):
 
 
     # Si la date est au format `Month DD, YYYY`, convertir le mois en son équivalent numérique (par exemple, "January" en "01").
-    elif re.match(pattern2, userdate, months, re.IGNORECASE):
+    elif re.match(pattern2, userdate, months, re.IGNORECASE) is True:
         month_word = pattern2.group(1)  # Mois en lettres
         month = months.index(month_word.title()) + 1  # Convertir le nom du mois en numéro
         day = pattern2.group(2)  # Jour
