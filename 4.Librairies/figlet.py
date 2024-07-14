dfb6aa069088eec7c5b75bf6a324bd0f1fbcad73
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-14 14:15:37 +0000

    Sun, Jul 14, 2024, 4:15 PM +02:00

--- 4.Librairies/figlet.py
+++ 4.Librairies/figlet.py
@@ -4,8 +4,11 @@ import sys
 
 def argvalid():  # vérifier que les arguments sont valides
     fonts = Figlet().getFonts()
-    if 1 < len(sys.argv) < 3 and sys.argv[2] in fonts: # vérifier la longueur des arguments et la validité de la font sys.argv[2]
-        return True
+    if 1 < len(sys.argv) < 3:
+
+        if sys.argv[2] not in fonts: # vérifier la longueur des arguments et la validité de la font sys.argv[2]
+            return False
+        elif sys.argv[1]
     else:
         return False
 
