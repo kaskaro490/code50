a66c66a80fbff7dbb4320ecaca58976439fb00b9
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-14 13:21:23 +0000

    Sun, Jul 14, 2024, 3:21 PM +02:00

--- 4.Librairies/figlet.py
+++ 4.Librairies/figlet.py
@@ -2,8 +2,11 @@ import pyfiglet
 import sys
 
 def main():
+    if sys.argv[2] is True:
+        font = sys.argv[2]
 
-    f = pyfiglet.figlet_format(input("Input: "), font=sys.argv[2])
+    else
+    f = pyfiglet.figlet_format(input("Input: "), font=)
     print(f)
 
 main()
