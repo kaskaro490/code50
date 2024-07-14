97bac6de23057bc151a8ea07919df16681f3d4b8
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-14 16:50:52 +0000

    Sun, Jul 14, 2024, 6:50 PM +02:00

--- 4.Librairies/figlet.py
+++ 4.Librairies/figlet.py
@@ -7,13 +7,15 @@ fonts = Figlet().getFonts()
 
 
 def argvalid():  # vérifier que les arguments sont valides
-    if len(sys.argv) < 4: # Vérifier que le nombre d'arguments et de 3
+    if len(sys.argv) == 3: # Vérifier que le nombre d'arguments et de 3
         if sys.argv[2] not in fonts: # vérifier la validité de la font sys.argv[2]
             return False
         elif sys.argv[1] != "-f" and sys.argv[1] != "--font": # vérifier la validité de sys.argv[1] qui doit être -f ou --font
             return False
         else:
             return True
+    elif len(sys.argv) == 0:
+        return True
     else:
         return sys.exit("bad font argument")
 
