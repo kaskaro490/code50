21b0861006e9a5a209a45bc188ec3a97cdccb5f5
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-14 20:12:23 +0000

    Sun, Jul 14, 2024, 10:12 PM +02:00

--- 4.Librairies/adieu.py
+++ 4.Librairies/adieu.py
@@ -15,11 +15,12 @@ def main():
         try:
             # Chaque prénom est incrémenté à une liste
             newname = input("Name: ")
-            names += newname
+            names.append(newname)
 
         # Lorsque ctrl+d est tapé, cela affiche "Adieu, adieu, to " suivi de chaque prénoms
         except EOFError:
             # Utilisation de .join() afin d'ajouter 'and' avant le dernier prénom de la liste
+            
 
 
 
