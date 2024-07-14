49db035d42c4d059ce656e1510f2e0b5b9e28c5a
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-14 13:53:39 +0000

    Sun, Jul 14, 2024, 3:53 PM +02:00

--- 4.Librairies/figlet.py
+++ 4.Librairies/figlet.py
@@ -5,12 +5,12 @@ def main():
     if 1 < len(sys.argv) <= 2:
 
         fonts = Figlet().getFonts()
-        if argv[2] not in fonts:
+        if sys.argv[2] not in fonts:
             f = Figlet(font = sys.argv[2])
-        elif:
-            f = Figlet(font = 'slant')
         else:
             sys.exit()
+    elif:
+            f = Figlet(font = 'slant')
 
     user = f.renderText(input('Input: '))
     print(user)
