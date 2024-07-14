5588e7d6c767482dbcd7b56f0163d909ff364b61
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-14 20:05:34 +0000

    Sun, Jul 14, 2024, 10:05 PM +02:00

--- 4.Librairies/adieu.py
+++ 4.Librairies/adieu.py
@@ -2,9 +2,22 @@ import inflect
 
 p = inflect.engine()
 
+prénoms = []
+
+
 
 
 def main():
+
+
+    # une boucle infinie qui demande à user d'écrire des prenoms
+    while True:
+        try:
+            # Chaque prénom est incrémenté à une liste
+
+
+        #Lorsque ctrl+d est tapé, cela affiche
+        except EOFError:
 
 
 
