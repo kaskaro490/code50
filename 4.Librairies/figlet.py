575132db90e53299d60d1e97c7d9448e603b091f
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-14 13:15:50 +0000

    Sun, Jul 14, 2024, 3:15 PM +02:00

--- 4.Librairies/figlet.py
+++ 4.Librairies/figlet.py
@@ -1,7 +1,7 @@
 import pyfiglet
 def main():
 
-    f = pyfiglet.figlet_format(input("Input"), font="slant")
+    f = pyfiglet.figlet_format(input("Input"), font="sys.argv[2]")
     print(f)
 
 main()
