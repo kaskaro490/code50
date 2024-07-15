9cbd869ff6a5fff4e705a30769623bd809537ad4
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-15 23:00:15 +0000

    Tue, Jul 16, 2024, 1:00 AM +02:00

--- 4.Librairies/professor.py
+++ 4.Librairies/professor.py
@@ -8,11 +8,10 @@ def main():
     good_answers = 0
     wrong_answers = 0
     while True:
-        levelselect = get_level("Level: ") # Demander à l'utilisateur de saisir un niveau (1, 2 ou 3) jusqu'à ce qu'une entrée valide soit reçue.
-        try:
 
+        try:
+            levelselect = get_level("Level: ") # Demander à l'utilisateur de saisir un niveau (1, 2 ou 3) jusqu'à ce qu'une entrée valide soit reçue.
             if not levelselect is False: # Vérifier la validité du niveau demandé.
-                    print("level ok")
                     try:
                         if good_answers + wrong_answers != 10: # Nombre de problèmes inférieur à 10
                             x = generate_integer(levelselect) # Générer un x aléatoire entre 0 et 9
