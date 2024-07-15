3d06b2389215fd938576d37c8c46b2bbcd07b6e9
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-15 22:34:26 +0000

    Tue, Jul 16, 2024, 12:34 AM +02:00

--- 4.Librairies/professor.py
+++ 4.Librairies/professor.py
@@ -11,6 +11,7 @@ def main():
         try:
             levelselect = get_level("Level: ") # Demander à l'utilisateur de saisir un niveau (1, 2 ou 3) jusqu'à ce qu'une entrée valide soit reçue.
             if not levelselect is False: # Vérifier la validité du niveau demandé.
+                print("level ok")
                 while True:
                     try:
                         if good_answers + wrong_answers != 10: # Nombre de problèmes inférieur à 10
