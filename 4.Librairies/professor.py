a037fd2fccb37cdc839585ad4fd82666754b1153
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-15 22:27:48 +0000

    Tue, Jul 16, 2024, 12:27 AM +02:00

--- 4.Librairies/professor.py
+++ 4.Librairies/professor.py
@@ -16,6 +16,7 @@ def main():
                         if good_answers + wrong_answers != 10: # Nombre de problèmes inférieur à 10
                             x = generate_integer(levelselect) # Générer un x aléatoire entre 0 et 9
                             y = generate_integer(levelselect) # Générer un y aléatoire entre 0 et 9
+
                             while attempts < 4: # Nombre de tentatives entre 1 et 3
                                 try:
                                     answer = int(input(f"{x}+{y}= "))
@@ -31,9 +32,7 @@ def main():
 
 
 
-                            else:
-
-
+                           
                         else:
                             print(f"Score: {good_answers}")
                             break
