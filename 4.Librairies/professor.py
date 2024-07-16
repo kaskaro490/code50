5d7a1300f56b72888a50f856ecee96e01444c3a8
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-16 15:27:54 +0000

    Tue, Jul 16, 2024, 5:27 PM +02:00

--- 4.Librairies/professor.py
+++ 4.Librairies/professor.py
@@ -21,7 +21,8 @@ def main():
                         answer = int(input(f"{x} + {y} = "))
 
                         if answer == (x + y):
-                            good_answers += 1
+                            good_answers += 118
+                            
                             break
                         elif attempts == 2:
                             wrong_answers += 1
