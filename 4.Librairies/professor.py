94349a38be17d049b7dd7a257d9de1e429aaa6f2
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-16 14:12:00 +0000

    Tue, Jul 16, 2024, 4:12 PM +02:00

--- 4.Librairies/professor.py
+++ 4.Librairies/professor.py
@@ -3,10 +3,11 @@ import sys
 
 def main():
 
-    n = int(get_level("Level: ")) # Prompt the user to enter a level (1, 2 or 3) until a valid entry is received.
+    level = int(get_level("Level: ")) # Prompt the user to enter a level (1, 2 or 3) until a valid entry is received.
     problems(n)
+    print(f"Score: {good_answers}")
 
-def problems(n):
+def problems(level):
     attempts = 0
     good_answers = 0
     wrong_answers = 0
@@ -14,8 +15,8 @@ def problems(n):
         try:
             if good_answers + wrong_answers != 10: # Number of problems less than 10
                 attempts = 0
-                x = generate_integer(n) # Generate a random x between 0 and 9
-                y = generate_integer(n) # Generate a random y between 0 and 9
+                x = generate_integer(level) # Generate a random x between 0 and 9
+                y = generate_integer(level) # Generate a random y between 0 and 9
 
                 while attempts < 4: # Number of attempts between 1 and 3
                     try:
@@ -37,8 +38,8 @@ def problems(n):
                     except:
                         pass
             else:
-                print(f"Score: {good_answers}")
-                sys.exit()
+                return good_answers
+
         except EOFError:
             break
         except ValueError:
@@ -46,22 +47,22 @@ def problems(n):
 
 
 
-def generate_integer(l): # Return a randomly generated non-negative integer with the number of digits specified according to the level.
+def generate_integer(level): # Return a randomly generated non-negative integer with the number of digits specified according to the level.
 
-    if l == 1:
+    if level == 1:
         return int(random.randint(0,9))
-    elif l == 2:
+    elif level == 2:
         return int(random.randint(10,99))
-    elif l ==3:
+    elif level ==3:
         return int(random.randint(100,999))
 
 def get_level(l): # Validate user level entry, returning 1, 2 or 3.
         while True:
             try:
-                n = int(input(l))
+                level = int(input(l))
 
                 if 0 < n < 4: # Check the validity of the level requested.
-                    return n
+                    return level
                 else:
                     raise ValueError
             except ValueError:
