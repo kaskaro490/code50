86217c0bea6de36f830cc12fb131de5dd1c55350
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-16 18:17:30 +0000

    Tue, Jul 16, 2024, 8:17 PM +02:00

--- 4.Librairies/bitcoin.py
+++ 4.Librairies/bitcoin.py
@@ -7,10 +7,15 @@ def main():
     if not len(sys.argv) != 2:
         try:
             n= float(sys.argv[1])
+            print("1")
             coindeskjson = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+            print("2")
             pythonjson = json.loads(coindeskjson)
+            print("3")
             currentprice = pythonjson["bpi"]["USD"]["rate_float"]
+            print("4")
             totalcost = currentprice * n
+            print("5")
             print(f"${amount:{totalcost},.4f}")
 
 
