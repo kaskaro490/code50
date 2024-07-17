7e90e1c4d012f971752f7e7972f648d3843afd66
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-17 14:19:50 +0000

    Wed, Jul 17, 2024, 4:19 PM +02:00

---
+++ 5.Unit_Tests/twttr.py
@@ -0,0 +1,15 @@
+def main():
+   vowels = ["a","e","i","o","u","A","E","I","O","U"]
+   userinput = input("Input: ")
+   #outputlist = twttr(input, vowels )
+   print(twttr(userinput, vowels))
+def twttr(userinput, vowels ):
+   outputlist = []
+   for letter in userinput:
+      if not letter in vowels:
+         outputlist.append(letter)
+
+   return ''. join(outputlist)
+
+main()
+
