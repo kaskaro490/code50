11357fc6929c5e86c643fc65114a6387333af77b
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-18 18:30:54 +0000

    Thu, Jul 18, 2024, 8:30 PM +02:00

--- 5.Unit_Tests/bank/bank.py
+++ 5.Unit_Tests/bank/bank.py
@@ -1,20 +1,21 @@
 
 
 def main():
-answer = input("Greeting: ").strip().lower()
-firstletter = answer[0]
-
-if answer == "hello" or answer.startswith("hello"):
-    print("$0")
-elif answer[0] == "h":
-    print("$20")
-else:
-    print("$100")
-
+    greeting = input("Greeting: ").strip().lower()
+²   value = value(greeting)
+    print(f"${value}")
 
 
 def value(greeting):
-    ...
+
+    firstletter = answer[0]
+    greeting = greeting.strip().lower()
+    if greeting == "hello" or greeting.startswith("hello"):
+        return 0
+    elif greeting[0] == "h":
+        return 20
+    else:
+        return 100
 
 
 if __name__ == "__main__":
