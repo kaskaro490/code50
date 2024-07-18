2aa3edf075ccc78944eb0fb79d96639e7f39e260
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-18 18:25:19 +0000

    Thu, Jul 18, 2024, 8:25 PM +02:00

--- 5.Unit_Tests/bank/bank.py
+++ 5.Unit_Tests/bank/bank.py
@@ -1,2 +1,21 @@
 
 
+def main():
+answer = input("Greeting: ").strip().lower()
+firstletter = answer[0]
+
+if answer == "hello" or answer.startswith("hello"):
+    print("$0")
+elif answer[0] == "h":
+    print("$20")
+else:
+    print("$100")
+
+
+
+def value(greeting):
+    ...
+
+
+if __name__ == "__main__":
+    main()
