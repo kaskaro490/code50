157976b9f9d93ea629d0f09da659a1f4ef66abcd
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-18 13:06:26 +0000

    Thu, Jul 18, 2024, 3:06 PM +02:00

--- 5.Unit_Tests/twttr.py
+++ 5.Unit_Tests/twttr.py
@@ -1,14 +1,12 @@
 def main():
    vowels = ["a","e","i","o","u","A","E","I","O","U"]
    word = input("Input: ")
-   #outputlist = twttr(input, vowels )
-   print(twttr(userinput, vowels))
+   print(shorten(word, vowels))
 
 
 
 def shorten(word, vowels):
 
-#def twttr(userinput, vowels ):
    outputlist = []
    for letter in word:
       if not letter in vowels:
