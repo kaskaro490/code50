e067fd76caa59733381be81276823262f6634fbc
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-18 13:48:38 +0000

    Thu, Jul 18, 2024, 3:48 PM +02:00

--- 5.Unit_Tests/test_twttr/test_twttr.py
+++ 5.Unit_Tests/test_twttr/test_twttr.py
@@ -3,4 +3,4 @@ from twttr import shorten
 def test_twttr():
     #vowels = ["a","e","i","o","u","A","E","I","O","U"]
     #words = ["twitter","autism","rocket","social","explicit","video","search","browser","minautor","maze"]
-    #assert shorten("twitter", vowels) == "twttr"
+    assert shorten("twitter", vowels) == "twttr"
