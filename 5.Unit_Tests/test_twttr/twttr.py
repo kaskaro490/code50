bbfc414befc32a9d97c0a6d3f19e1c9ce18ad8de
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-18 14:16:02 +0000

    Thu, Jul 18, 2024, 4:16 PM +02:00

--- 5.Unit_Tests/test_twttr/twttr.py
+++ 5.Unit_Tests/test_twttr/twttr.py
@@ -1,18 +1,17 @@
 def main():
-   vowels = ["a","e","i","o","u","A","E","I","O","U"]
-   word = input("Input: ")
-   print(shorten(word, vowels))
+    word = input("Input: ")
+    print(shorten(word))
 
+def shorten(word, vowels=None):
+    if vowels is None:
+        vowels = ["a","e","i","o","u","A","E","I","O","U"]
 
+    outputlist = []
+    for letter in word:
+        if letter not in vowels:
+            outputlist.append(letter)
 
-def shorten(word, vowels):
-
-   outputlist = []
-   for letter in word:
-      if not letter in vowels:
-         outputlist.append(letter)
-
-   return ''.join(outputlist)
+    return ''.join(outputlist)
 
 if __name__ == "__main__":
     main()
