3c6ae33e8bda4bed9eae8f26c2d1c693b9182609
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-19 16:43:30 +0000

    Fri, Jul 19, 2024, 6:43 PM +02:00

--- 5.Unit_Tests/test_plates/test_plates.py
+++ 5.Unit_Tests/test_plates/test_plates.py
@@ -1,24 +1,21 @@
 from plates import is_valid
 
+platenames = ["OUTATIME","H","CS50","CS50","PI3.14","CS05P","","","",""]
 
 def test_is_valid(s):
-    assert is_valid("CS50") == True
-    assert is_valid("CS05") == False
-    assert is_valid("CS50P") == False
-    assert is_valid("PI3.14") == False
-    assert is_valid("H") == True
-    assert is_valid("OUTATIME") == True
-    assert is_valid("CS50") == True
+ for
+
 
 def test_check_length(s):
+    assert is_valid("") == True
 
-    assert is_valid("OUTATIME") == True
 
 def test_check_start_letters(s):
-    assert is_valid("CS50") == True
+    assert is_valid("") == True
 
 def test_check_characters(s):
-    assert is_valid("CS50") == True
+    assert is_valid("") == True
+
 
 def test_check_numbers(s):
-    assert is_valid("CS05P") == False
+    assert is_valid("") == True
