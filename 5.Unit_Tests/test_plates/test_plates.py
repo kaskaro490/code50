2f393707245b1b11e56bf07e9ab6dd81af87496e
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-19 16:36:57 +0000

    Fri, Jul 19, 2024, 6:36 PM +02:00

--- 5.Unit_Tests/test_plates/test_plates.py
+++ 5.Unit_Tests/test_plates/test_plates.py
@@ -1,13 +1,21 @@
 from plates import is_valid
 
 
-def test_is_valid():
+def test_is_valid(s):
     assert is_valid("CS50") == True
     assert is_valid("CS05") == False
     assert is_valid("CS50P") == False
-    assert is_valid("CS50") == True
-    assert is_valid("CS50") == True
-    assert is_valid("CS50") == True
+    assert is_valid("PI3.14") == False
+    assert is_valid("H") == True
+    assert is_valid("OUTATIME") == True
     assert is_valid("CS50") == True
 
-def test_check_length():
+def test_check_length(s):
+
+    assert is_valid("OUTATIME") == True
+
+def test_check_start_letters(s):
+    assert is_valid("CS50") == True
+
+def test_check_characters(s):
+    
