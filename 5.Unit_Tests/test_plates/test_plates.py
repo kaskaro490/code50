5eb7c0ab2b1573895a93559bac2bc0ee715234cf
kaskaro490 <59427852+kaskaro490@users.noreply.github.com>
2024-07-19 16:39:16 +0000

    Fri, Jul 19, 2024, 6:39 PM +02:00

--- 5.Unit_Tests/test_plates/test_plates.py
+++ 5.Unit_Tests/test_plates/test_plates.py
@@ -18,4 +18,7 @@ def test_check_start_letters(s):
     assert is_valid("CS50") == True
 
 def test_check_characters(s):
-    
+    assert is_valid("CS50") == True
+
+def test_check_numbers(s):
+    assert is_valid("CS05P") == False
